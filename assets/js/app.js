d5bfd70c67133ce5eb26427e47581d801e3a5f6b
Graham Neal <nealjg@gmail.com>
2018-03-24 13:54:57 -0400

    removed redundant map variable

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,8 +20,6 @@ $(document).ready(function () {
     // Create a storage reference from our storage service
     var storageRef = storage.ref();
 
-    var map = L.map('map').fitWorld();
-
 
     var mapBox = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
